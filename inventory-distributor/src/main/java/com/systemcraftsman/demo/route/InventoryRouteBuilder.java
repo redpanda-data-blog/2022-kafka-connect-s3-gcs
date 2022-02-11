238143c015bc59be79bc4d30ed195ec3ca646afd
mabulgu <abulgu@redhat.com>
2022-02-11 03:13:51 +0300

    add producer & s3 sink connector

--- inventory-distributor/src/main/java/com/systemcraftsman/demo/route/InventoryRouteBuilder.java
+++ inventory-distributor/src/main/java/com/systemcraftsman/demo/route/InventoryRouteBuilder.java
@@ -1,14 +1,40 @@
 package com.systemcraftsman.demo.route;
 
-import org.apache.camel.builder.RouteBuilder;
+import java.util.TimeZone;
 
+import javax.enterprise.context.ApplicationScoped;
+
+import com.systemcraftsman.demo.model.BookInfo;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.camel.model.dataformat.BindyType;
+
+@ApplicationScoped
 public class InventoryRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 
-        from("file:...")
-            .to("kafka:london-inventory");
+        from("file://../resources/data?fileName=book-inventory.csv&noop=true&idempotentKey=${file:name}-${file:modified}")
+            .transform(body())
+            .unmarshal()
+            .bindy(BindyType.Csv, BookInfo.class)
+            .split(body()).parallelProcessing()
+            .setHeader(KafkaConstants.KEY, simple("${body.isbn}"))
+            .choice()
+                .when(simple("${body.storeLocation} == 'london'"))
+                    .marshal(new JacksonDataFormat(BookInfo.class))
+                    .to("kafka:london-inventory")
+                .when(simple("${body.storeLocation} == 'newyork'"))
+                    .marshal(new JacksonDataFormat(BookInfo.class))
+                    .to("kafka:newyork-inventory")
+                .otherwise()
+                    .log("No store location is defined for ${body}");
+            ;
     }
 }
 
